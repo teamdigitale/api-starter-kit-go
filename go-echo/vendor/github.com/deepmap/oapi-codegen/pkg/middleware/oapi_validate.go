5e2670608f39dcdb138b5ad14a7129b906610439
axamon <alberto.bregliano@gmail.com>
2019-12-10 12:05:45 +0100

    added vendor dir with all dependencies

---
+++ go-echo/vendor/github.com/deepmap/oapi-codegen/pkg/middleware/oapi_validate.go
@@ -0,0 +1,152 @@
+// Copyright 2019 DeepMap, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/getkin/kin-openapi/openapi3filter"
+	"github.com/labstack/echo/v4"
+)
+
+const EchoContextKey = "oapi-codegen/echo-context"
+const UserDataKey = "oapi-codegen/user-data"
+
+// This is an Echo middleware function which validates incoming HTTP requests
+// to make sure that they conform to the given OAPI 3.0 specification. When
+// OAPI validation failes on the request, we return an HTTP/400.
+
+// Create validator middleware from a YAML file path
+func OapiValidatorFromYamlFile(path string) (echo.MiddlewareFunc, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("error reading %s: %s", path, err)
+	}
+
+	swagger, err := openapi3.NewSwaggerLoader().LoadSwaggerFromData(data)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing %s as Swagger YAML: %s",
+			path, err)
+	}
+	return OapiRequestValidator(swagger), nil
+}
+
+// Create a validator from a swagger object.
+func OapiRequestValidator(swagger *openapi3.Swagger) echo.MiddlewareFunc {
+	return OapiRequestValidatorWithOptions(swagger, nil)
+}
+
+// Options to customize request validation. These are passed through to
+// openapi3filter.
+type Options struct {
+	Options      openapi3filter.Options
+	ParamDecoder openapi3filter.ContentParameterDecoder
+	UserData     interface{}
+}
+
+// Create a validator from a swagger object, with validation options
+func OapiRequestValidatorWithOptions(swagger *openapi3.Swagger, options *Options) echo.MiddlewareFunc {
+	router := openapi3filter.NewRouter().WithSwagger(swagger)
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			err := ValidateRequestFromContext(c, router, options)
+			if err != nil {
+				return err
+			}
+			return next(c)
+		}
+	}
+}
+
+// This function is called from the middleware above and actually does the work
+// of validating a request.
+func ValidateRequestFromContext(ctx echo.Context, router *openapi3filter.Router, options *Options) error {
+	req := ctx.Request()
+	route, pathParams, err := router.FindRoute(req.Method, req.URL)
+
+	// We failed to find a matching route for the request.
+	if err != nil {
+		switch e := err.(type) {
+		case *openapi3filter.RouteError:
+			// We've got a bad request, the path requested doesn't match
+			// either server, or path, or something.
+			return echo.NewHTTPError(http.StatusBadRequest, e.Reason)
+		default:
+			// This should never happen today, but if our upstream code changes,
+			// we don't want to crash the server, so handle the unexpected error.
+			return echo.NewHTTPError(http.StatusInternalServerError,
+				fmt.Sprintf("error validating route: %s", err.Error()))
+		}
+	}
+
+	validationInput := &openapi3filter.RequestValidationInput{
+		Request:    req,
+		PathParams: pathParams,
+		Route:      route,
+	}
+
+	// Pass the Echo context into the request validator, so that any callbacks
+	// which it invokes make it available.
+	requestContext := context.WithValue(context.Background(), EchoContextKey, ctx)
+
+	if options != nil {
+		validationInput.Options = &options.Options
+		validationInput.ParamDecoder = options.ParamDecoder
+		requestContext = context.WithValue(requestContext, UserDataKey, options.UserData)
+	}
+
+	err = openapi3filter.ValidateRequest(requestContext, validationInput)
+	if err != nil {
+		switch e := err.(type) {
+		case *openapi3filter.RequestError:
+			// We've got a bad request
+			// Split up the verbose error by lines and return the first one
+			// openapi errors seem to be multi-line with a decent message on the first
+			errorLines := strings.Split(e.Error(), "\n")
+			return echo.NewHTTPError(http.StatusBadRequest, errorLines[0])
+		case *openapi3filter.SecurityRequirementsError:
+			return echo.NewHTTPError(http.StatusForbidden, e.Error())
+		default:
+			// This should never happen today, but if our upstream code changes,
+			// we don't want to crash the server, so handle the unexpected error.
+			return echo.NewHTTPError(http.StatusInternalServerError,
+				fmt.Sprintf("error validating request: %s", err))
+		}
+	}
+	return nil
+}
+
+// Helper function to get the echo context from within requests. It returns
+// nil if not found or wrong type.
+func GetEchoContext(c context.Context) echo.Context {
+	iface := c.Value(EchoContextKey)
+	if iface == nil {
+		return nil
+	}
+	eCtx, ok := iface.(echo.Context)
+	if !ok {
+		return nil
+	}
+	return eCtx
+}
+
+func GetUserData(c context.Context) interface{} {
+	return c.Value(UserDataKey)
+}
